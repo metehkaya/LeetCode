00bc995f625bf8f383767663edaaa0e3ca13c13e
Metehan Kaya <metehkaya96@gmail.com>
2023-05-31 01:09:25 +0200

    Added some JS problems

---
+++ Solutions/JavaScript/2628.JSON_Deep_Equal.js
@@ -0,0 +1,12 @@
+function helper(key, value) {
+    if (value && typeof value === "object" && !Array.isArray(value))
+        return Object.fromEntries(Object.entries(value).sort());
+    else
+        return value;
+}
+
+var areDeeplyEqual = function(o1, o2) {
+    const stringifiedO1 = JSON.stringify(o1, helper);
+    const stringifiedO2 = JSON.stringify(o2, helper);
+    return stringifiedO1 === stringifiedO2;
+};
