7caf7e1ee65c9ba4d329493238a3c177db0087ce
Metehan Kaya <metehkaya96@gmail.com>
2023-06-03 21:22:37 -0400

    Added a few JS problems

---
+++ Solutions/JavaScript/2636.Promise_Pool.js
@@ -0,0 +1,10 @@
+var promisePool = async function(functions, n) {
+    async function evaluateNext() {
+        if (functions.length === 0) return;
+        const fn = functions.shift();
+        await fn();
+        await evaluateNext();
+    }
+    const nPromises = Array(n).fill().map(evaluateNext);
+    await Promise.all(nPromises);
+};
